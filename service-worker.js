f12e6b89aa7b313e2241bfa765c415a322c0c7d2
Berndk77 <s9k8t8pq5p@privaterelay.appleid.com>
2025-11-08 11:33:39 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,16 @@
 self.addEventListener('install', (e)=>{
   e.waitUntil(
-    caches.open('apy-cache-v1').then(cache => cache.addAll([
+    caches.open('apy-cache-v2').then(cache => cache.addAll([
       './index.html',
       './manifest.webmanifest'
     ]))
   );
 });
-
+self.addEventListener('activate', (e)=>{
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k=>k!=='apy-cache-v2').map(k=>caches.delete(k))))
+  );
+});
 self.addEventListener('fetch', (e)=>{
   e.respondWith(
     caches.match(e.request).then(resp => resp || fetch(e.request))
